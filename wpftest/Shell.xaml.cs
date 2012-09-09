0233e84d9b505b3faadf0754028e1558aceffda2
biluochun2010 <biluochun2010@yahoo.com>
2012-09-10 07:18:53 +0800

    rename

--- wpftest/Shell.xaml.cs
+++ wpftest/Shell.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 namespace wpftest
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Shell.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Shell : Window
     {
-        public MainWindow()
+        public Shell()
         {
             InitializeComponent();
         }
