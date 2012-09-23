6409172faa5aae16e6966ac1daf92b714ad20ae2
biluochun2010 <biluochun2010@yahoo.com>
2012-09-23 09:14:34 +0800

    add mef project

---
+++ wpfMEF/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.MefExtensions;
+
+namespace wpftest
+{
+    class Bootstrapper : MefBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureAggregateCatalog()
+        {
+            base.ConfigureAggregateCatalog();
+         
+        }
+    }
+}
