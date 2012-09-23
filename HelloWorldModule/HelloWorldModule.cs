10c431f919f0f3186a16497fff7d763ed2fda61d
biluochun2010 <biluochun2010@yahoo.com>
2012-09-23 08:19:50 +0800

    Add more stuff

---
+++ HelloWorldModule/HelloWorldModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+
+namespace HelloWorldModule 
+{
+    public class HelloWorldModule : IModule
+    {
+        public HelloWorldModule(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        private readonly IRegionManager regionManager;
+
+        public void Initialize()
+        {
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(HelloWorldView));
+        }
+    }
+}
---
+++ wpftest/Bootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Prism.UnityExtensions;
+using System.Windows;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace wpftest
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(HelloWorldModule.HelloWorldModule));
+
+        }
+    }
+}
