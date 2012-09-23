198927a4fd79076d6f3c1d95d3aa8557a0564351
biluochun2010 <biluochun2010@yahoo.com>
2012-09-23 17:28:20 +0800

    adding MVVM example

--- HelloWorldModule/ViewModels/HelloWorldViewModel.cs
+++ HelloWorldModule/ViewModels/HelloWorldViewModel.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.ComponentModel.Composition;
+using Microsoft.Practices.Prism.ViewModel;
 
 namespace HelloWorldModule.ViewModels
 {
-    [Export]
-    class HelloWorldViewModel
+    [Export(typeof(HelloWorldViewModel))]
+    class HelloWorldViewModel : NotificationObject
     {
+        private List<String> resources = new List<String>();
+
+        [ImportingConstructor]
+        public HelloWorldViewModel()
+        {
+            resources.Add("One");
+            resources.Add("Two");
+            resources.Add("Three");
+        }
+
+        public List<String> ResourceList
+        {
+            get {
+                return resources;
+            }
+        }
     }
 }
--- HelloWorldModule/Views/HelloWorldView.xaml.cs
+++ HelloWorldModule/Views/HelloWorldView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.ComponentModel.Composition;
+using HelloWorldModule.ViewModels;
 
 namespace HelloWorldModule.Views
 {
@@ -24,6 +25,12 @@ namespace HelloWorldModule.Views
         public HelloWorldView()
         {
             InitializeComponent();
+        }
+
+        [Import]
+        HelloWorldViewModel ViewModel
+        {
+            set { this.DataContext = value; }
         }
     }
 }
